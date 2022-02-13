088a486739dc3ccf50a76185e48a1e05cb3b3625
dangmnn <dangmnn@gmail.com>
2022-02-13 12:21:10 +0700

    init

---
+++ Day01/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arrNum = {10, 15, 3, 7};
+int k = 18;
+
+for (var i = 0; i < arrNum.Length; i++)
+{
+    for (var j = i + 1; j < arrNum.Length; j++)
+    {
+        if (arrNum[i] + arrNum[j] == k)
+        {
+            System.Console.WriteLine(arrNum[i] + " + " + arrNum[j] + " = " + k);
+            break;
+        }
+    }
+}
