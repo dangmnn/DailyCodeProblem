81f3d94f3e08b292a201f42558d20155eb6154fd
dangmnn <dangmnn@gmail.com>
2022-02-13 13:53:55 +0700

    init

---
+++ Day02/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arrNum = {1,2,3,4,5};
+int[] result = new int[arrNum.Length];
+
+for (var i = 0; i < arrNum.Length; i++)
+{
+    var temp = 1;
+    for (var j = 0 ; j < arrNum.Length; j++)
+    {
+        if(arrNum[i] != arrNum[j])
+        {
+            temp = temp * arrNum[j];
+        }
+    }
+    result[i] = temp;
+}
+
+for (var i = 0; i < result.Length; i++)
+{
+    System.Console.WriteLine(result[i]);
+}
